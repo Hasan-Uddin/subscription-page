5b18de12552f5f751a954e769b0dcf64ecec49fe
Hasan Uddin <abcnizamd@gmail.com>
2025-10-16 11:46:23 +0600

    update-1

---
+++ src/app/components/plan-list/plan-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-list',
+  imports: [],
+  templateUrl: './plan-list.html',
+  styleUrl: './plan-list.css'
+})
+export class PlanList {
+
+}
--- src/app/components/subscription-modal/subscription-modal.ts
+++ src/app/components/subscription-modal/subscription-modal.ts
@@ -2,22 +2,39 @@ import { Component, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { SubscriptionPlan } from '../../models/subscription.model';
 import { SubscriptionService } from '../../services/subscription.services';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-subscription-modal',
-  imports: [],
   templateUrl: './subscription-modal.html',
+  imports: [CommonModule, FormsModule],
   styleUrl: './subscription-modal.css',
 })
 export class SubscriptionModal {
   @Input() plan!: SubscriptionPlan;
 
+  smsOption: 'off' | 'on' = 'off';
+  smsUnitPrice: number = 0.6;
+  smsQuantity: number = 0;
+  additionalSmsCost: number = 0;
+  totalPrice: number = 0;
+
   constructor(public activeModal: NgbActiveModal, private subService: SubscriptionService) {}
+
+  ngOnInit() {
+    this.calculateTotal(); // initialize total price
+  }
+
+  calculateTotal() {
+    this.additionalSmsCost = this.smsOption === 'on' ? this.smsQuantity * this.smsUnitPrice : 0;
+    this.totalPrice = this.plan.price + this.additionalSmsCost;
+  }
 
   close() {
     this.activeModal.dismiss();
   }
 
-  // For Testing, change here for back end call
   confirmPurchase() {
     const now = new Date();
     const expiry = new Date();
@@ -25,7 +42,7 @@ export class SubscriptionModal {
 
     this.subService.addToHistory({
       planName: this.plan.name,
-      amount: this.plan.price,
+      amount: this.totalPrice,
       paymentDate: now.toLocaleDateString(),
       expiryDate: expiry.toLocaleDateString(),
       status: 'Active',
